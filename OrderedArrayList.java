2418be6091ca5839c36020cf5e15a2751dbf2740
sging20 <sging20@stuy.edu>
2020-12-13 20:15:54 -0500

    Made sort method and add method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,4 +7,27 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
       public OrderedArrayList(int initialCapacity){
         super(initialCapacity);
     }
+  private int sort(T element){
+    if(this.size()== 0) return 0;
+    int index= 0;
+        for(int i=0; i< this.size(); i++){
+          int getter= get(i).compareTo(element);
+            if(getter < 0){
+              index++;
+              }
+              else{
+                return index;
+              }
+            }
+            return index;
+  }
+
+
+  public void add(int index, T element){
+    if(element == null){
+        throw new IllegalArgumentException("Illegal Argument" + element);
+    }
+      super.add(sort(element), element);
+  }
+
 }
